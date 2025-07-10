87911d0ea8fdaa9313ee564cb0f6dc04cc51c4c7
calebewerneckcouto <calebewerneck@hotmail.com>
2025-07-10 15:45:11 -0300

    modificações necessarias projeto

--- src/main/java/com/cwcdev/blog/model/Blog.java
+++ src/main/java/com/cwcdev/blog/model/Blog.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 
 @Entity
 public class Blog {
@@ -13,6 +14,7 @@ public class Blog {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	private String title;
+	@Lob
 	private String body;
 	
 	public Blog() {
